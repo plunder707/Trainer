38afeffd39bc32dd11195a93f94fe543054b524d
plunder707 <125803170+plunder707@users.noreply.github.com>
2023-10-24 18:44:58 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,38 +1,39 @@
 import argparse
-import logging
+import json
+import optuna
 import sys
 import torch
+import wandb
 from datasets import load_dataset, concatenate_datasets, load_metric
-from transformers import AutoModelForCausalLM, AutoTokenizer, Trainer, TrainingArguments, get_linear_schedule_with_warmup
-from haystack.document_stores import ElasticsearchDocumentStore
-from haystack.retriever.dense import EmbeddingRetriever
-from haystack.pipeline import ExtractiveQAPipeline
+from elasticsearch import Elasticsearch
+from optuna.integration import WeightsAndBiasesCallback
+from optuna.samplers import MultivariateTPESampler
+from transformers import (AdamW, AutoModelForCausalLM, AutoTokenizer,
+                          EarlyStoppingCallback, Trainer, TrainingArguments,
+                          get_linear_schedule_with_warmup)
 
-# Initialize logging
-logging.basicConfig(level=logging.INFO)
+wandb.init(project="LLaMAM-Pipeline-Enhanced")
 
 class LLaMATrainingPipeline:
-    def __init__(self, model_path="distilbert-base-uncased", device="cuda"):
+    def __init__(self, model_path, device):
         self.model_path = model_path
-        self.set_device(device)
+        self.device = device
         self.model = AutoModelForCausalLM.from_pretrained(self.model_path).to(self.device)
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_path)
-        self.metrics = {
-            "rouge": load_metric("rouge"),
-            "meteor": load_metric("meteor")
-        }
-        self.document_store = ElasticsearchDocumentStore(host="localhost", username="", password="", index="document")
-        self.retriever = EmbeddingRetriever(document_store=self.document_store, embedding_model=model_path, use_gpu=True)
+        self.metrics = {"rouge": load_metric("rouge"), "meteor": load_metric("meteor")}
+        self.es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
-    def set_device(self, device):
-        self.device = device
-        if self.device not in ["cuda", "cpu"]:
-            logging.error("Invalid device specified. Using CPU.")
-            self.device = "cpu"
+    def create_and_ingest_index(self, index_name, filepath):
+        self.es.indices.create(index=index_name, ignore=400)
+        with open(filepath, 'r') as f:
+            data = json.load(f)
+            for i, doc in enumerate(data):
+                self.es.index(index=index_name, id=i, body=doc)
 
-    def load_dataset(self, dataset_name="squad"):
-        self.dataset_train = load_dataset(dataset_name, split="train")
-        self.dataset_valid = load_dataset(dataset_name, split="validation")
+    def load_dataset(self, dataset_name, download):
+        if download:
+            self.dataset_train = load_dataset(dataset_name, split="train")
+            self.dataset_valid = load_dataset(dataset_name, split="validation")
         self.dataset = concatenate_datasets([self.dataset_train, self.dataset_valid])
 
     def tokenize_function(self, examples):
@@ -47,74 +48,94 @@ class LLaMATrainingPipeline:
         meteor = self.metrics['meteor'].compute(predictions=predictions_text, references=labels_text)
         return {"rouge": rouge, "meteor": meteor}
 
-def main(args):
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    training_pipeline = LLaMATrainingPipeline(args.model_path, device)
-    training_pipeline.load_dataset(args.dataset_name)
-    tokenized_dataset = training_pipeline.dataset.map(training_pipeline.tokenize_function, batched=True, num_proc=4)
+def objective(trial):
+    wandb.init()
+    learning_rate = trial.suggest_loguniform("learning_rate", 1e-6, 1e-4)
+    num_train_epochs = trial.suggest_int("num_train_epochs", 1, 5)
+    model_path = trial.suggest_categorical("model_path", ["bert-base-uncased", "roberta-base"])
+    per_device_train_batch_size = trial.suggest_categorical("per_device_train_batch_size", [8, 16, 32])
+    weight_decay = trial.suggest_float("weight_decay", 0, 0.1)
+    warmup_steps = trial.suggest_int("warmup_steps", 0, 1000)
+    adam_epsilon = trial.suggest_float("adam_epsilon", 1e-9, 1e-7)
+    adam_beta1 = trial.suggest_float("adam_beta1", 0.8, 0.95)
+    adam_beta2 = trial.suggest_float("adam_beta2", 0.98, 0.999)
+
+    pipeline = LLaMATrainingPipeline(model_path, "cuda")
+    pipeline.load_dataset("squad", download=True)
+    tokenized_dataset = pipeline.dataset.map(pipeline.tokenize_function, batched=True, num_proc=4)
 
     training_args = TrainingArguments(
-        output_dir=args.output_dir,
-        num_train_epochs=args.num_train_epochs,
-        per_device_train_batch_size=8,
+        output_dir="results",
+        num_train_epochs=num_train_epochs,
+        per_device_train_batch_size=per_device_train_batch_size,
         per_device_eval_batch_size=8,
         gradient_accumulation_steps=2,
-        warmup_steps=args.warmup_steps,
-        weight_decay=args.weight_decay,
-        logging_dir=args.logging_dir,
-        logging_steps=args.logging_steps,
-        save_total_limit=args.save_total_limit,
+        warmup_steps=warmup_steps,
+        weight_decay=weight_decay,
+        logging_dir="./logs",
+        logging_steps=100,
+        save_total_limit=5,
         load_best_model_at_end=True,
         metric_for_best_model="rouge",
         greater_is_better=True,
         evaluation_strategy="steps",
         save_strategy="steps",
-        learning_rate=5e-5,
+        learning_rate=learning_rate,
         fp16=True,
         remove_unused_columns=False,
-        dataloader_num_workers=4
+        dataloader_num_workers=4,
+        report_to="wandb",
+        push_to_hub=False,
+        deepspeed="ds_config.json",
+        callbacks=[EarlyStoppingCallback(early_stopping_patience=3, early_stopping_threshold=0.05)]
     )
 
-    # Learning rate scheduler
+    optimizer = AdamW(params=pipeline.model.parameters(), lr=learning_rate, betas=(adam_beta1, adam_beta2), eps=adam_epsilon)
+    
     scheduler = get_linear_schedule_with_warmup(
-        optimizer=torch.optim.AdamW(params=training_pipeline.model.parameters(), lr=5e-5),
-        num_warmup_steps=args.warmup_steps,
-        num_training_steps=len(tokenized_dataset) // training_args.per_device_train_batch_size
+        optimizer,
+        num_warmup_steps=warmup_steps,
+        num_training_steps=len(tokenized_dataset) // per_device_train_batch_size
     )
 
     trainer = Trainer(
-        model=training_pipeline.model,
+        model=pipeline.model,
         args=training_args,
         train_dataset=tokenized_dataset,
-        data_collator=lambda data: {
-            'input_ids': torch.stack([f['input_ids'] for f in data]),
-            'attention_mask': torch.stack([f['attention_mask'] for f in data]),
-            'labels': torch.stack([f['labels'] for f in data]),
-        },
-        compute_metrics=training_pipeline.compute_metrics,
-        optimizer=torch.optim.AdamW(params=training_pipeline.model.parameters(), lr=5e-5),
+        data_collator=lambda data: {'input_ids': torch.stack([f['input_ids'] for f in data]),
+                                    'attention_mask': torch.stack([f['attention_mask'] for f in data]),
+                                    'labels': torch.stack([f['labels'] for f in data])},
+        compute_metrics=pipeline.compute_metrics,
+        optimizer=optimizer,
         lr_scheduler=scheduler,
-        accelerator="dp"
+        accelerator="ddp"
     )
 
-    # Actual training
     trainer.train()
-
-    # Clear GPU cache
     torch.cuda.empty_cache()
+
+    eval_dataset = load_dataset("squad", split="test")
+    eval_result = trainer.evaluate(eval_dataset)
+    wandb.log(eval_result)
+    return eval_result["eval_rouge"]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_path', type=str, default="distilbert-base-uncased", help='Path to the pre-trained model')
-    parser.add_argument('--dataset_name', type=str, default="squad", help='Name of the dataset to use')
-    parser.add_argument('--output_dir', type=str, default="results", help='Directory to save training results')
-    parser.add_argument('--num_train_epochs', type=int, default=3, help='Number of training epochs')
-    parser.add_argument('--warmup_steps', type=int, default=500, help='Warm-up steps for scheduler')
-    parser.add_argument('--weight_decay', type=float, default=0.01, help='Weight decay for optimizer')
-    parser.add_argument('--logging_dir', type=str, default="./logs", help='Directory for logs')
-    parser.add_argument('--logging_steps', type=int, default=100, help='Logging steps')
-    parser.add_argument('--save_total_limit', type=int, default=5, help='Total limit of saved models')
+    parser.add_argument('--index_name', type=str, required=True)
+    parser.add_argument('--filepath', type=str, required=True)
+    parser.add_argument('--download_dataset', type=bool, default=False)
     args = parser.parse_args()
 
-    main(args)
+    pipeline = LLaMATrainingPipeline("distilbert-base-uncased", "cuda")
+    pipeline.create_and_ingest_index(args.index_name, args.filepath)
 
+    study = optuna.create_study(direction="maximize", sampler=MultivariateTPESampler(), callbacks=[WeightsAndBiasesCallback()])
+    study.optimize(objective, n_trials=100)
+
+    print(f"Number of finished trials: {len(study.trials)}")
+    print("Best trial:")
+    trial = study.best_trial
+    print(f"Value: {trial.value}")
+    print("Params: ")
+    for key, value in trial.params.items():
+        print(f"{key}: {value}")
